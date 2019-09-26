d9d87371f920870041a7da88215a540d7da068f4
Nguyen_Quoc_Cuong <quoccuongdn96vn@gmail.com>
2019-09-26 10:32:46 +0700

    Commit the first time for new branch

---
+++ JSP05/src/jsp05_day1/NewBranch.java
@@ -0,0 +1,10 @@
+package jsp05_day1;
+
+public class NewBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test a new branch");
+	}
+
+}
